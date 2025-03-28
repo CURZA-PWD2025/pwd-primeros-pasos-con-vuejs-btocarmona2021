2de60c7456d119f51c463cfe4dd5ef28776c8788
Alberto C <btocarmona@gmail.com>
2025-03-27 23:04:36 -0300

    Tp-01 Terminado agregado algo de Estilo

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+// Definición de variables reactivas.
 const nombre = ref("");
+const focusNombre = ref(false);
 const email = ref("");
+const focusEmail = ref(false);
 const password = ref();
+const focusPassword = ref(false);
 const fecha = ref(new Date().toISOString().substr(0, 10));
+const focusFecha = ref(false);
 const passwordEncriptado = ref("");
 
 const submitForm = () => {
+if (!nombre.value || !email.value || !password.value || !fecha.value) {
+    alert("Todos los campos son obligatorios");
+    return;
+  }
   const usuario = {
     nombre: nombre.value,
     email: email.value,
@@ -36,28 +45,46 @@ const limpiarForm = () => {
   email.value = "";
   password.value = "";
   fecha.value = "";
+  focusNombre.value = false;
+  focusEmail.value = false;
+  focusPassword.value = false;
+  focusFecha.value = false;
+};
+const focus = (e:FocusEvent) => {
+  const target = e.target as HTMLInputElement;
+  if (target.placeholder === "Nombre"){
+    focusNombre.value = true;
+  }else if (target.placeholder === "Email"){
+    focusEmail.value = true; 
+  }else if (target.placeholder === "Password"){
+    focusPassword.value = true;
+  }else if (target.placeholder === "Fecha"){
+    focusFecha.value = true;
+  }
 };
 </script>
 
 <template>
   <div class="main">
     <form class="main__form" @submit.prevent="submitForm">
-      <input type="text" v-model="nombre" placeholder="Nombre" />
-      <input type="email" v-model="email" placeholder="Email" />
+      <input type="text" v-model="nombre" placeholder="Nombre" @focus=focus />
+      <input type="email" v-model="email" placeholder="Email" @focus=focus />
       <input
         type="password"
         v-model="password"
         @input="encriptarPassword"
         placeholder="Password"
+        @focus=focus 
       />
-      <input type="date" v-model="fecha" />
+      <input type="date" v-model="fecha" placeholder="Fecha" @focus=focus />
       <button type="submit">Enviar</button>
     </form>
     <div class="main__data">
-      <p>Nombre: {{ nombre }}</p>
-      <p>Email: {{ email }}</p>
-      <p>Password:{{ passwordEncriptado }}</p>
-      <p>Edad: {{ calcularEdad() }} años</p>
+      <p><span v-if="focusNombre">✔</span> Nombre: <span>{{ nombre }}</span></p>
+      <p><span v-if="focusEmail">✔</span> Email: <span>{{ email }}</span></p>
+      <p><span v-if="focusPassword">✔</span> Password:<span>{{ passwordEncriptado }}</span></p>
+      <p><span v-if="focusFecha">✔</span> Edad: <span>{{ calcularEdad() }} año/s</span></p>
+
     </div>
   </div>
 </template>
@@ -88,7 +115,14 @@ input {
   margin-bottom: 1rem;
   font-family: "Chakra Petch", sans-serif;
   font-size: 1.1rem;
+  outline: none;
 }
+input:focus-within {
+  outline-color: rgb(63, 0, 88) 2px solid;
+  box-shadow: 0 0 10px 10px rgba(94, 71, 133);
+  outline: rgb(171, 0, 238) 2px solid; ;
+}
+
 .main__data {
   display: flex;
   flex-direction: column;
@@ -100,5 +134,9 @@ input {
   border-radius: 8px;
   font-family: "Chakra Petch", sans-serif;
   font-size: 1.1rem;
+}
+span {
+  font-weight: bold;
+  color:  rgb(171, 0, 238);
 }
 </style>
